6268fe43bcf9a38f998c9a6957dc1500dfb7d93d
Michael Melesse <micmelesse@gmail.com>
2020-05-23 23:48:04 -0700

    add docker file

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,7 @@
-FROM tensorflow/tensorflow:1.15.2-gpu-py3-jupyter
+FROM tensorflow/tensorflow:1.15.2-gpu-py3
+RUN apt-get update && apt-get install -y \
+    libsm6 \
+    libxext6 \
+    libxrender-dev
 COPY ./requirements.txt /
 RUN pip3 install -r /requirements.txt
--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -1,4 +1,4 @@
 PWD=`pwd`
-# alias drun="docker run -it --rm --network=host -v $PWD:$PWD -w $PWD" 
-alias nv_drun="docker run --ipc=host -it --rm --network=host --runtime=nvidia -v $PWD:$PWD -w $PWD"
+alias nv_drun="docker run -it --rm --network=host -v $PWD:$PWD -w $PWD" 
+# alias nv_drun="docker run --ipc=host -it --rm --network=host --runtime=nvidia -v $PWD:$PWD -w $PWD"
 nv_drun selflow
